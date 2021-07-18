0da33165b22b145852b67fb846494f0844967bfd
Raghvendra Singh <Raghav26897@gmail.com>
2021-07-18 20:17:53 +0530

    Done

---
+++ react-api-call/src/Data.js
@@ -0,0 +1,5 @@
+const ImgData=[
+    {id:"1",Img:"https://gamedata.britishcouncil.org/sites/default/files/attachment/number-1_1.jpg"}
+]
+
+export default ImgData;
---
+++ react-api-call/src/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ImgData from "./Data"
+
+const Home = () => {
+    return (
+        <div>
+            <h1></h1>
+        </div>
+    )
+}
+
+export default Home;
